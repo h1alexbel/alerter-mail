b7bdfef592909d73e703feffc0a0c701ddfc8ab7
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-02 19:39:32 +0300

    src(src): updated MailServiceImpl;

--- src/main/java/com/alerter/mail/service/impl/MailServiceImpl.java
+++ src/main/java/com/alerter/mail/service/impl/MailServiceImpl.java
@@ -3,8 +3,12 @@ package com.alerter.mail.service.impl;
 import com.alerter.mail.core.MailRq;
 import com.alerter.mail.core.PreparedMail;
 import com.alerter.mail.service.MailService;
+import java.nio.charset.StandardCharsets;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,11 +23,28 @@ public class MailServiceImpl implements MailService {
 
   @Override
   public PreparedMail prepare(MailRq request) {
-    throw new UnsupportedOperationException("#prepare()");
+    String response;
+    MimeMessage message = sender.createMimeMessage();
+    try {
+      MimeMessageHelper helper =
+          new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+              StandardCharsets.UTF_8.name());
+      helper.setTo(request.getTo());
+      helper.setFrom(request.getFrom());
+      helper.setSubject(request.getSubject());
+      String alerter =
+          "Automated with Alerter (https://github.com/h1alexbel);";
+      String rultor = "DevOps with Rultor(https://github.com/yegor256/rultor)";
+      String skip = "-------------------------------------";
+      helper.setText(request.getDocLink() + "\n" + "\n" + skip + "\n" + alerter + "\n" + rultor);
+    } catch (final MessagingException e) {
+      throw new IllegalStateException("", e);
+    }
+    return new PreparedMail(message);
   }
 
   @Override
   public void send(PreparedMail mail) {
-    throw new UnsupportedOperationException("#send()");
+    this.sender.send(mail.getMessage());
   }
 }
