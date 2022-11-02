f8b27f42b37410913019a4ddba241058a8c6146d
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-02 18:54:57 +0300

    src(src): added AlerterMailApplication;

---
+++ src/main/java/com/alerter/mail/AlerterMailApplication.java
@@ -0,0 +1,12 @@
+package com.alerter.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlerterMailApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlerterMailApplication.class, args);
+  }
+}
