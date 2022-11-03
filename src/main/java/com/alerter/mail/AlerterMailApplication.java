fa7115d2bf44a21f221d572a92e5b10aceae36a0
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-03 19:33:03 +0300

    feat(schedule ping): added schedule ping logic;

--- src/main/java/com/alerter/mail/AlerterMailApplication.java
+++ src/main/java/com/alerter/mail/AlerterMailApplication.java
@@ -1,12 +1,33 @@
 package com.alerter.mail;
 
+import com.alerter.mail.trigger.TrResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @SpringBootApplication
+@RequiredArgsConstructor
 public class AlerterMailApplication {
+
+  private final RestTemplate rest;
 
   public static void main(String[] args) {
     SpringApplication.run(AlerterMailApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner runner() {
+    return args -> this.ping();
+  }
+
+  @Scheduled(fixedDelay = 10000L)
+  private void ping() {
+    this.rest.getForEntity("https://tg-alerter.herokuapp.com/api/v1/trigger", TrResponse.class);
   }
 }
