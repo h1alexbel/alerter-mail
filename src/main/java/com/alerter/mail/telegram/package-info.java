e3d1a9a63f7f472fefe07172e604534f9e3b3daa
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-21 19:59:29 +0300

    added docs, package-info.java;

--- src/main/java/com/alerter/mail/telegram/package-info.java
+++ src/main/java/com/alerter/mail/telegram/package-info.java
@@ -18,33 +18,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-package com.alerter.mail.sender;
-
-import com.alerter.mail.model.Mail;
-import javax.mail.internet.MimeMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
 /**
+ * Telegram.
  * @author Aliaksei Bialiauski (abialiauski@solvd.com)
- * @since 0.0.1
+ * @since 1.0
  */
-public final class SendMail implements Mail {
 
-  private final MimeMessage message;
-  private final JavaMailSender sender;
-
-  public SendMail(final MimeMessage message, final JavaMailSender sender) {
-    this.message = message;
-    this.sender = sender;
-  }
-
-  public SendMail send() {
-    this.sender.send(this.message);
-    return this;
-  }
-
-  @Override
-  public MimeMessage mime() {
-    return this.message;
-  }
-}
+package com.alerter.mail.telegram;
