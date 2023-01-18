21233337bb7e5a62ce3c9efc9ba97abbe65e0f7c
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-01-18 14:27:14 +0300

    renamed builder method;

--- src/main/java/com/alerter/mail/telegram/TgDocumentURL.java
+++ src/main/java/com/alerter/mail/telegram/TgDocumentURL.java
@@ -1,21 +1,21 @@
 /*
-MIT License
-Copyright (c) 2022 Aliaksei Bialiauski
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ * MIT License
+ * Copyright (c) 2022 Aliaksei Bialiauski
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.alerter.mail.telegram;
@@ -26,6 +26,8 @@ import com.pengrad.telegrambot.model.Document;
 import com.pengrad.telegrambot.request.GetFile;
 
 /**
+ * Telegram document url.
+ *
  * @author Aliaksei Bialiauski (abialiauski@solvd.com)
  * @since 0.0.1
  */
@@ -34,19 +36,25 @@ public final class TgDocumentURL implements Message<String> {
   private final Document document;
   private final TelegramBot bot;
 
-  public TgDocumentURL(final Document doc, final TelegramBot bot) {
+  /**
+   * Ctor.
+   *
+   * @param doc document Document
+   * @param bt  bot
+   */
+  public TgDocumentURL(final Document doc, final TelegramBot bt) {
     this.document = doc;
-    this.bot = bot;
+    this.bot = bt;
   }
 
   @Override
-  public String read() {
+  public String content() {
     return this.bot.getFullFilePath(
-        this.bot.execute(
-            new GetFile(
-                this.document.fileId()
-            )
-        ).file()
+      this.bot.execute(
+        new GetFile(
+          this.document.fileId()
+        )
+      ).file()
     );
   }
 }
